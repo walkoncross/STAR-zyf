558666a32f5b692d86175b5136ff1672378b621c
zhaoyafei <zhaoyafei0210@gmail.com>
2025-01-16 02:29:31 +0200

    fix absolute ckpt_dir in conf/alignment.py

--- conf/alignment.py
+++ conf/alignment.py
@@ -9,7 +9,7 @@ class Alignment(Base):
 
     def __init__(self, args):
         super(Alignment, self).__init__('alignment')
-        self.ckpt_dir = '/apdcephfs_cq3/share_1134483/charlinzhou/ckpts/STAR/'
+        self.ckpt_dir = './ckpts/STAR/'
         self.net = "stackedHGnet_v1"
         self.nstack = 4
         self.loader_type = "alignment"
